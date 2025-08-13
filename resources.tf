18273db61b8cbcbb0b78047359c4e87924aafb06
ghostlygeorge <georgejoe.gs@gmail.com>
2025-08-13 14:10:46 +0300

    Add third subnet and tags

--- resources.tf
+++ resources.tf
@@ -17,6 +17,8 @@ data "aws_availability_zones" "available" {}
 ##################################################################################
 locals {
   common_tags = {
+    Environment = var.envirnonment
+    BillingCode = var.billing_code
 
   }
 }
